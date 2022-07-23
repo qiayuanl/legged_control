8efdfc679796343d1ff6f22008e9146044b0e07b
qiayuan <liaoqiayuan@gmail.com>
2022-07-24 00:18:38 +0800

    Get TIME_TO_TARGET from horizon of mpc

--- legged_controllers/src/cmd_vel_node.cpp
+++ legged_controllers/src/cmd_vel_node.cpp
@@ -14,6 +14,7 @@ scalar_t TARGET_DISPLACEMENT_VELOCITY;
 scalar_t TARGET_ROTATION_VELOCITY;
 scalar_t COM_HEIGHT;
 vector_t DEFAULT_JOINT_STATE(12);
+scalar_t TIME_TO_TARGET;
 }  // namespace
 
 TargetTrajectories cmdVelToTargetTrajectories(const vector_t& cmd_vel, const SystemObservation& observation)
@@ -59,13 +60,15 @@ int main(int argc, char* argv[])
   ::ros::init(argc, argv, robot_name + "_target");
   ::ros::NodeHandle node_handle;
   // Get node parameters
-  std::string reference_file;
+  std::string reference_file, task_file;
   node_handle.getParam("/reference_file", reference_file);
+  node_handle.getParam("/task_file", task_file);
 
   loadData::loadCppDataType(reference_file, "comHeight", COM_HEIGHT);
   loadData::loadEigenMatrix(reference_file, "defaultJointState", DEFAULT_JOINT_STATE);
   loadData::loadCppDataType(reference_file, "targetRotationVelocity", TARGET_ROTATION_VELOCITY);
   loadData::loadCppDataType(reference_file, "targetDisplacementVelocity", TARGET_DISPLACEMENT_VELOCITY);
+  loadData::loadCppDataType(task_file, "mpc.timeHorizon", TIME_TO_TARGET);
 
   TargetTrajectoriesGoalPublisher target_pose_command(node_handle, robot_name, &cmdVelToTargetTrajectories);
 
